72cef4f1efb6ea30018aeab2c71e7b1e4801e1a2
JunHoPaeng <93682556+bugbuger97@users.noreply.github.com>
2023-09-10 17:09:10 +0900

    [Bronze III] Title: 최소, 최대, Time: 380 ms, Memory: 150220 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,5 +1,6 @@
+import sys
 if __name__ == '__main__':
-  N = int(input())  
-  numbers = input()
+  N = int(sys.stdin.readline())  
+  numbers = sys.stdin.readline()
   num = list(map(int,numbers.split()))
   print(f'{min(num)} {max(num)}')
