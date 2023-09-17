d1ee549d90a7869fa7765d5ab8642804f1c66474
JunHoPaeng <93682556+bugbuger97@users.noreply.github.com>
2023-09-17 20:54:50 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,14 +1,11 @@
 if __name__ == '__main__':
   T = int(input())
+  devide = [25,10,5,1]
   result = []
   for i in range(T):
     C = int(input())
-    result.append(C // 25)
-    C%=25
-    result.append(C//10)
-    C%=10
-    result.append(C//5)
-    C%=5
-    result.append(C//1)
+    for j in devide:
+      result.append(C//j)
+      C%=j
     print(*result)
     result = []
