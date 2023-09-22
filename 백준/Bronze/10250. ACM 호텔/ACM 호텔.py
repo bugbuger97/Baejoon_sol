4d7775cbe75881b5e9d9a678cbfeb488c7ca43e9
JunHoPaeng <93682556+bugbuger97@users.noreply.github.com>
2023-09-23 04:26:12 +0900

    [Bronze III] Title: ACM 호텔, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,18 +1,1 @@
-def Room_number(H,W,N) -> str:
-  matrix = []
-  temp = []
-  for i in range(W):
-    for j in range(H):
-      if i+1 < 10:
-        temp.append(str(j+1)+'0'+str(i+1))
-      else:
-        temp.append(str(j+1)+str(i+1))
-    matrix+=temp
-    temp = []
-  return matrix[N-1] 
-
-if __name__ == '__main__':
-  T = int(input())
-  for i in range(T):
-    H,W,N = map(int,input().split()) # H = 층 수, W = 방 개수, N = 몇 번째 손님
-    print(Room_number(H,W,N))
+for i in range(int(input())):H,W,N=map(int,input().split());N-=1;print(N%H*100+N//H+101)
