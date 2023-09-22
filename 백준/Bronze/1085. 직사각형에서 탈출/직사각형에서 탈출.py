add8194725fa10ee5d24403f3df5074a4a18dd0e
JunHoPaeng <93682556+bugbuger97@users.noreply.github.com>
2023-09-23 04:44:17 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,5 +1,2 @@
-def distanse(x,y,w,h):
-  return ((x-w)**2 +(y - h) **2) ** 0.5
-x,y,w,h = map(int,input().split())
-result = [distanse(x,y,0,0),distanse(x,y,w,0),distanse(x,y,0,h),distanse(x,y,w,h),abs(x-w),abs(y-h),x,y]
-print(min(result))
+x,y,w,h=map(int,input().split())
+print(min(x,y,h-y,w-x))
