dd99aebc715f0e2cadcef480ae1f49130914f79b
JunHoPaeng <93682556+bugbuger97@users.noreply.github.com>
2024-04-06 20:05:09 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -1,14 +1,14 @@
 #include <iostream> 
+#include <string>
 using namespace std;
 
 int main() {
+	string star = "";
 	int num;
 	cin >> num;
-	for (int i = 1; i <= num; i++) { // for(초기화; 반복 조건; 증감식)
-		for (int j = 0; j < i; j++) {
-			cout << '*';
-		}
-		cout << endl;
+	for (int i = 1; i <= num; i++) { 
+		star += "*";
+		cout << star << endl;
 	}
 	return 0;
 }
